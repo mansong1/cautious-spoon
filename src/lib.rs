f42bacf8bc6b565283b3d46954dc269b810b1753
Martin Ansong <martin.ansong@protonmail.com>
2021-07-29 12:34:21 +0100

    fix fmt errors

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! lib.rs
 
-use actix_web::{web, App, HttpRequest, HttpServer, HttpResponse, Responder};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
@@ -17,7 +17,7 @@ pub async fn run() -> std::io::Result<()> {
             .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
-        })
+    })
     .bind("127.0.0.1:8000")?
     .run()
     .await
