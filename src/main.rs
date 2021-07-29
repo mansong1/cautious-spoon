42043844c75fcdd4923c414f599195147141f636
Martin Ansong <martin.ansong@protonmail.com>
2021-07-29 12:32:54 +0100

    restructuring

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,8 @@
-use actix_web::{web, App, HttpRequest, HttpServer, HttpResponse, Responder};
+//! main.rs
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use api::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-            .route("/health_check", web::get().to(health_check))
-        })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    run().await;
 }
