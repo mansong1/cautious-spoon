b4d5904d1054542d21ea9470e11aa71d148c2626
Martin Ansong <martin.ansong@protonmail.com>
2021-07-29 16:47:14 +0100

    use ip addr instead of localhost

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -5,22 +5,18 @@ async fn health_check_works() {
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://localhost:8080/health_check")
+        .get("http://127.0.0.1:8000/health_check")
         .send()
         .await
         .expect("Failed to get health check.");
 
     // Assert
-    assert!(response.status_code().is_success());
+    assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
 
-async fn spawn_app() -> std::io::Result<()> {
-    api::run().await
-}
-
 fn spawn_app() {
-    let server = api::run().await.expect("Failed to bind address.");
+    let server = api::run().expect("Failed to bind address.");
 
     let _ = tokio::spawn(server);
 }
