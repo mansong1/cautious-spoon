51626491d453efe410ce637103635775a145e969
Martin Ansong <martin.ansong@protonmail.com>
2021-08-05 14:16:44 +0100

    revert changes

--- src/lib.rs
+++ src/lib.rs
@@ -15,9 +15,9 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
-// async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-//     HttpResponse::Ok().finish()
-// }
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
